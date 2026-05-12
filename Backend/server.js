7aba22e7c7fd89c35fc7b90fb0df176327777165
Aryan Vaghela <aryanvaghela248@gmail.com>
2026-05-12 10:43:01 +0530

    added render url

--- Backend/server.js
+++ Backend/server.js
@@ -25,10 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], // Allows both just in case
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'atoken', 'dtoken', 'Authorization', 'token'],
-  credentials: true
+  credentials: false
 }));
 
 //Api endpoint
